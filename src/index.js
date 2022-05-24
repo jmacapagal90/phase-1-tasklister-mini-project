9d5fd7bc837b56347a10efa58d392d85a9dfdefe
James Macapagal <j.macapagal@gmail.com>
2022-05-24 09:55:15 -0500

    commit changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleToDo(e.target.querySelector("input").value)
+    }
+  )
 });
+
+function handleToDo (todo){
+  const ul = document.getElementById("tasks")
+  let li  = document.createElement("li")
+  let btn = document.createElement('button')
+  btn.addEventListener('click',handleDelete)
+  btn.textContent = "x"
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  ul.appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
